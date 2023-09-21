ea8ca8405875ba539fa762f10c60cd56ef839410
Prajwal Rathod <88572430+prarathod@users.noreply.github.com>
2023-09-21 10:21:43 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (75.53%) - LeetSync

--- leetcode/1983-maximum-population-year/maximum-population-year.java
+++ leetcode/1983-maximum-population-year/maximum-population-year.java
@@ -1,9 +1,6 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
         int[] year = new int[2051];
-        
-		// O(n) -> n is log.length
-		
         for(int[] log : logs){
             
             year[log[0]] += 1;
@@ -11,11 +8,9 @@ class Solution {
         }
         
         int maxNum = year[1950], maxYear = 1950;
-        
-		// O(100) -> 2050 - 1950 = 100
 
         for(int i = 1951; i < year.length; i++){
-            year[i] += year[i - 1];  // Generating Prefix Sum
+            year[i] += year[i - 1];
             
             if(year[i] > maxNum){
                 maxNum = year[i];
